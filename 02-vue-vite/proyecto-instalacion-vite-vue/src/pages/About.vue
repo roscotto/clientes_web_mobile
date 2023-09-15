9ea1be20cfa4cc1073265a4df46be3e999a30c14
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-15 20:51:42 -0300

    creacion de componentes Home / About

---
+++ 02-vue-vite/proyecto-instalacion-vite-vue/src/pages/About.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        name: 'About',
+    }
+</script>
+
+<template>
+    <h1 class="mb-4 text-3xl font-bold">Quiénes somos</h1>
+</template>
---
+++ 02-vue-vite/proyecto-instalacion-vite-vue/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        name: 'Home',
+    }
+</script>
+
+<template>
+    <h1 class="mb-4 text-3xl font-bold">Página Principal</h1>
+</template>
