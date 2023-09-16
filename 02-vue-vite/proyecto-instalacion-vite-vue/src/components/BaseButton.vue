07db2b0c939edb35498c379c92a97dc6626d51e5
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-16 19:51:02 -0300

    transition & motion-reduce

--- 02-vue-vite/proyecto-instalacion-vite-vue/src/components/BaseButton.vue
+++ 02-vue-vite/proyecto-instalacion-vite-vue/src/components/BaseButton.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-    <button class="bg-blue-700 text-white p-1.5 rounded-xl w-full" type="submit" name="enviar" value="Enviar">
+    <button class="bg-blue-700 text-white p-1.5 rounded-xl w-full hover:bg-blue-600 shadow-sm active:bg-blue-800 transition motion-reduce:transition-none motion-reduce:hover:transform-none " type="submit" name="enviar" value="Enviar">
         Enviar
     </button>
 </template>
--- 02-vue-vite/proyecto-instalacion-vite-vue/src/pages/Register.vue
+++ 02-vue-vite/proyecto-instalacion-vite-vue/src/pages/Register.vue
@@ -1,6 +1,9 @@
 <script>
+import BaseButton from '../components/BaseButton.vue';
+
 export default {
     name: 'Register',
+    components: { BaseButton }
 }
 </script>
 
@@ -16,6 +19,7 @@ export default {
             <label for="password" class="block font-bold mb-1">Contraseña</label>
             <input type="password" name="password" id="password" class="border border-gray-500 rounded p-2 w-full">
         </div>
-        <button type="submit" class="bg-blue-700 text-white p-1.5 rounded-xl w-full">Crear Cuenta</button>
+        <!-- <button type="submit" class="bg-blue-700 text-white p-1.5 rounded-xl w-full">Crear Cuenta</button> -->
+        <BaseButton />
     </form>
 </template>
